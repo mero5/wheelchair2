5f5b7c1f05ea9e11330d3504302fe8085c50ff4c
mero5 <kksw-2824@outlook.jp>
2023-04-25 05:56:00 +0000

    readme修正

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module Wheelchair2
 
     config.i18n.default_locale = :ja
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
